c5cdfcd585ea60412a8f05c2a0980891b907b0e7
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-11-21 17:35:08 +0100

    Torch wrapper device

--- rl_transformer/wrappers.py
+++ rl_transformer/wrappers.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Optional, Tuple, Union
 
 import gym
 import torch
@@ -10,11 +10,15 @@ class TorchWrapper(gym.Wrapper):
     Torch wrapper. Actions and observations are Tensors instead of arrays.
     """
 
+    def __init__(self, env: gym.Env, device: Optional[Union[torch.device, str]] = None) -> None:
+        super().__init__(env)
+        self.device = device
+
     def step(self, action: Tensor) -> Tuple[Tensor, float, bool, Dict[str, Any]]:
-        action = action.numpy()
+        action = action.cpu().numpy()
         observation, reward, done, info = self.env.step(action)
-        return torch.tensor(observation), reward, done, info
+        return torch.tensor(observation).to(self.device), reward, done, info
 
     def reset(self) -> Tensor:
         observation = self.env.reset()
-        return torch.tensor(observation)
+        return torch.tensor(observation).to(self.device)
