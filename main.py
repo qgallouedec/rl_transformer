9f8e54afde4ca20210d74cb4ea66b027f8e255d9
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-11-21 18:10:02 +0100

    Update main

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import gym
-import torch
 
 from rl_transformer.buffer import EpisodeBuffer
 from rl_transformer.rl_transformer import ActorCriticTransformer
+from rl_transformer.wrappers import TorchWrapper
 
-env = gym.make("Pendulum-v1")
-
-buffer = EpisodeBuffer(20, env)
-ac = ActorCriticTransformer(env, d_model=16)
+device = "cuda"
+env = TorchWrapper(gym.make("Pendulum-v1"), device)
+buffer = EpisodeBuffer(20, env, device)
+ac = ActorCriticTransformer(env, d_model=16).to(device)
 
 observation = env.reset()
-buffer.new_episode(observation)
+value = ac.get_value(observation)
+buffer.new_episode(observation, value)
 for _ in range(3000):
     observations, actions = buffer.get_current_episode()
-    action = ac.act(torch.from_numpy(observations), torch.from_numpy(actions)).numpy()
+    value, action, log_prob = ac.act(observations, actions)
     observation, reward, done, info = env.step(action)
-    buffer.add(action, observation, reward, done, info)
+    buffer.add(action, log_prob, observation, value, reward, done, info)
     if done:
         observation = env.reset()
-        buffer.new_episode(observation)
+        value = ac.get_value(observation)
+        buffer.new_episode(observation, value)
 
-observations, actions, rewards, done, infos, src_key_padding_mask = buffer.sample(5)
-observations = torch.from_numpy(observations)
-actions = torch.from_numpy(actions)
-src_key_padding_mask = torch.from_numpy(src_key_padding_mask)
-actions, values, log_prob = ac.forward(observations, actions, src_key_padding_mask)
+# observations, actions, rewards, done, infos, src_key_padding_mask = buffer.sample(5)
+# observations = torch.from_numpy(observations)
+# actions = torch.from_numpy(actions)
+# src_key_padding_mask = torch.from_numpy(src_key_padding_mask)
+# actions, values, log_prob = ac.forward(observations, actions, src_key_padding_mask)
 
-print(actions)
+# print(actions)
