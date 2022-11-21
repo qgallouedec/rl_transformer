4a9456c8a4143aae42af61387f4ef5a70f7e99df
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-11-21 14:03:59 +0100

    Fix torch wrapper for discrete actions

--- rl_transformer/wrappers.py
+++ rl_transformer/wrappers.py
@@ -7,14 +7,14 @@ from torch import Tensor
 
 class TorchWrapper(gym.Wrapper):
     """
-    Torch wrapper. Actions and observations are Tensors instead of arrays
+    Torch wrapper. Actions and observations are Tensors instead of arrays.
     """
 
     def step(self, action: Tensor) -> Tuple[Tensor, float, bool, Dict[str, Any]]:
         action = action.numpy()
         observation, reward, done, info = self.env.step(action)
-        return torch.from_numpy(observation), reward, done, info
+        return torch.tensor(observation), reward, done, info
 
     def reset(self) -> Tensor:
         observation = self.env.reset()
-        return torch.from_numpy(observation)
+        return torch.tensor(observation)
