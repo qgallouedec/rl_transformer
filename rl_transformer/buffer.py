830e25091298d06a3b40c8bb0713a1d3bd47609c
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-11-21 11:21:45 +0100

    numpy to torch dtype un utils

--- rl_transformer/buffer.py
+++ rl_transformer/buffer.py
@@ -5,18 +5,7 @@ import numpy as np
 import torch
 from torch import Tensor
 
-numpy_to_torch_dtype = {
-    np.uint8: torch.uint8,
-    np.int8: torch.int8,
-    np.int16: torch.int16,
-    np.int32: torch.int32,
-    np.int64: torch.int64,
-    np.float16: torch.float16,
-    np.float32: torch.float32,
-    np.float64: torch.float64,
-    np.complex64: torch.complex64,
-    np.complex128: torch.complex128,
-}
+from rl_transformer.utils import NUMPY_TO_TORCH_DTYPE
 
 
 class EpisodeBuffer:
@@ -39,10 +28,10 @@ class EpisodeBuffer:
         action_shape = env.action_space.shape
         action_dtype = env.action_space.dtype.type
         self.observations = torch.zeros(
-            (buffer_size, self.max_ep_len, *observation_shape), dtype=numpy_to_torch_dtype[observation_dtype]
+            (buffer_size, self.max_ep_len, *observation_shape), dtype=NUMPY_TO_TORCH_DTYPE[observation_dtype]
         )
         self.values = torch.zeros((buffer_size, self.max_ep_len), dtype=float)
-        self.actions = torch.zeros((buffer_size, self.max_ep_len, *action_shape), dtype=numpy_to_torch_dtype[action_dtype])
+        self.actions = torch.zeros((buffer_size, self.max_ep_len, *action_shape), dtype=NUMPY_TO_TORCH_DTYPE[action_dtype])
         self.log_probs = torch.zeros((buffer_size, self.max_ep_len), dtype=float)
         self.rewards = torch.zeros((buffer_size, self.max_ep_len), dtype=float)
         self.dones = torch.zeros((buffer_size, self.max_ep_len), dtype=bool)
--- rl_transformer/utils.py
+++ rl_transformer/utils.py
@@ -3,6 +3,19 @@ import torch
 import torch.nn.functional as F
 from gym import spaces
 
+NUMPY_TO_TORCH_DTYPE = {
+    np.uint8: torch.uint8,
+    np.int8: torch.int8,
+    np.int16: torch.int16,
+    np.int32: torch.int32,
+    np.int64: torch.int64,
+    np.float16: torch.float16,
+    np.float32: torch.float32,
+    np.float64: torch.float64,
+    np.complex64: torch.complex64,
+    np.complex128: torch.complex128,
+}
+
 
 def get_space_size(space: spaces.Space) -> int:
     """Return de size of the space.
