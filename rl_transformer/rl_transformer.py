7d2de4b93e8a599209bb6aa5f943369674c0f011
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-11-21 17:38:01 +0100

    better vectorize AC and make it devicable

--- rl_transformer/rl_transformer.py
+++ rl_transformer/rl_transformer.py
@@ -7,7 +7,7 @@ from gym import spaces
 from torch import Tensor, nn
 from torch.distributions import Categorical, Normal
 
-from rl_transformer.utils import get_space_size, preprocess
+from rl_transformer.utils import get_space_shape, get_space_size, preprocess
 
 
 class PositionalEncoder(nn.Module):
@@ -43,13 +43,13 @@ class PositionalEncoder(nn.Module):
         Add the positional encoding to the input.
 
         Args:
-            x (Tensor): Input of shape (N, L, H_in)
+            x (Tensor): Input of shape (..., L, H_in)
 
         Returns:
             Tensor: Output of shape (N, L, H_in). It corresponds to the
                 input, to which we have added the positional encoding
         """
-        seq_len = x.shape[1]
+        seq_len = x.shape[-2]
         x = x + self.pe[:seq_len]
         return self.dropout(x)
 
@@ -81,7 +81,8 @@ class ActorCriticTransformer(nn.Module):
         self.positional_encoder = PositionalEncoder(d_model, max_len, dropout)
         encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dropout=dropout, batch_first=True)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers)
-        self.mask = generate_square_subsequent_mask(max_len)
+        mask = generate_square_subsequent_mask(max_len)
+        self.register_buffer("mask", mask)
         self.value_net = nn.Sequential(
             nn.Linear(d_model, d_model),
             nn.ReLU(),
@@ -98,20 +99,22 @@ class ActorCriticTransformer(nn.Module):
         Compute the  predicted actions and values given the episodes.
 
         Args:
-            observations (Tensor): Observation of shape (N, L+1, obs_size). Ending with s_t.
-            actions (Tensor): Actions of shape (N, L, action_size). Ending with a_{t-1}
+            observations (Tensor): Observation of shape (..., L+1, obs_shape). Ending with s_t.
+            actions (Tensor): Actions of shape (..., L, action_shape). Ending with a_{t-1}
             deterministic (bool): If actor acts deterinistially
 
         Returns:
             Predicted actions, values and log_prob
         """
-        observations = preprocess(observations, self.observation_space)
-        actions = preprocess(actions, self.action_space)
-        actions = torch.hstack((torch.zeros(actions.shape[0], 1, actions.shape[2]), actions))  # (N, L, *) to (N, L+1, *)
-        observations_actions = torch.dstack((observations, actions))  # (N, L+1, observation_size + action_size)
+        observations = preprocess(observations, self.observation_space)  # (..., L+1, *obs_shape) -> (..., L+1, obs_size)
+        actions = preprocess(actions, self.action_space)  # (..., L, *action_shape) -> (..., L, action_size)
+        input_shape = observations.shape[:-1]  # = (..., L)
+        pad = torch.zeros(*actions.shape[:-2], 1, actions.shape[-1]).to(actions.device)
+        actions = torch.concatenate((pad, actions), dim=-2)  # (..., L, action_size) to (..., L+1, action_size)
+        observations_actions = torch.concatenate((observations, actions), dim=-1)  # (..., L+1, observation_size + action_size)
         x = self.input_encoder(observations_actions)
         x = self.positional_encoder(x)
-        mask = self.mask[: x.size(1), : x.size(1)]
+        mask = self.mask[: x.size(-2), : x.size(-2)]
         x = self.transformer_encoder(x, mask, src_key_padding_mask)
         # Critic
         values = self.value_net(x)
@@ -132,38 +135,45 @@ class ActorCriticTransformer(nn.Module):
             else:
                 actions = torch.argmax(distribution.probs, dim=-1)
         if isinstance(self.action_space, spaces.Box):
-            actions = torch.clip(actions, torch.tensor(self.action_space.low), torch.tensor(self.action_space.high))
+            actions = torch.clip(
+                actions,
+                torch.tensor(self.action_space.low).to(actions.device),
+                torch.tensor(self.action_space.high).to(actions.device),
+            )
         log_prob = distribution.log_prob(actions)
-        return actions, values, log_prob
+        return values.reshape((*input_shape, -1)), actions.reshape((*input_shape, -1)), log_prob.reshape((*input_shape, -1))
 
-    def act(self, observations: Tensor, actions: Tensor, deterministic: bool = False) -> Tensor:
+    def act(self, observations: Tensor, actions: Tensor, deterministic: bool = False) -> Tuple[Tensor, Tensor, Tensor]:
         """
-        Compute the  predicted action given the episode.
+        Compute the predicted action given the episode.
 
         Args:
-            observations (Tensor): Observation of shape (L+1, obs_size). Ending with s_t.
-            actions (Tensor): Actions of shape (L, action_size). Ending with a_{t-1}
+            observations (Tensor): Past observation of shape (L+1, obs_shape). Ending with s_t.
+            actions (Tensor): Past actions of shape (L, action_shape). Ending with a_{t-1}
             deterministic (bool): If actor acts deterinistially
 
         Returns:
             Predicted next action
         """
-        observations = observations.unsqueeze(0)  # (L, obs_size) -> (1, L, obs_size)
-        actions = actions.unsqueeze(0)  # (L, action_size) -> (1, L, action_size)
         with torch.no_grad():
-            actions, _, _ = self.forward(observations, actions, deterministic=deterministic)
-        return actions[0][-1]  # get only the last action
+            values, actions, log_prob = self.forward(observations, actions, deterministic=deterministic)
+        return values[-1], actions[-1], log_prob[-1]  # get only the last action
 
+    def get_value(self, observations: Tensor) -> Tensor:
+        """
+        Compute the value of the current observation.
 
-if __name__ == "__main__":
-    import gym
+        Args:
+            observations (Tensor): Past observation of shape (L+1, obs_shape). Ending with s_t.
+            actions (Tensor): Past actions of shape (L, action_shape). Ending with a_{t-1}
 
-    env = gym.make("Pendulum-v1")
-    ac = ActorCriticTransformer(env, d_model=16)
-
-    obs_size = env.observation_space.shape[0]
-    action_size = env.action_space.shape[0]
-    observations = torch.rand((13, obs_size))
-    actions = torch.rand((12, action_size))
-
-    print(ac.act(observations, actions))
+        Returns:
+            Value
+        """
+        obs_shape = get_space_shape(self.observation_space)
+        action_space = get_space_shape(self.action_space)
+        observations = observations.reshape((1, *obs_shape))  # (obs_shape,) -> (1, obs_shape)
+        actions = torch.zeros(0, *action_space).to(observations.device)  # (0, action_shape)
+        with torch.no_grad():
+            values, _, _ = self.forward(observations, actions)
+        return values[0]  # get only the last value
